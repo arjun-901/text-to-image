0d100141bc2b17bff3c48a4b18cbc0e7f664580b
arjun-901 <arjun9654abc@gmail.com>
2025-02-01 15:05:57 +0530

    added description  about section

--- clint/src/components/Description.jsx
+++ clint/src/components/Description.jsx
@@ -14,9 +14,9 @@ const Description = () => {
             <div className='text-center'>
                 <h2 className='text-3xl font-medium max-w-lg'>Intoducing the AI-powered Text to Image Generator.</h2><br />
                 <h2>
-                    <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde placeat nihil quae suscipit voluptatum accusamus. Amet sunt, consequatur modi odit.</p>
+                    <p className='text-gray-800'>Create stunning images directly from text with our Text-to-Image Generator. Simply input your text, and the tool converts it into a beautiful, styled image, preserving the layout, font, color, and spacing of the original design. </p>
                     <br />
-                    <p className='text-gray-800'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam et enim neque facere laboriosam iusto perferendis excepturi tempore veniam </p>
+                    <p className='text-gray-800'>Perfect for social media posts, presentations, and more. Whether you want to share quotes, stories, or custom messages, our tool brings your words to life in a visually appealing way.</p>
                 </h2>
             </div>
         </div>
